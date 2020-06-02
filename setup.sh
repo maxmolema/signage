1def0ea537ba642e15dc6f8a7e24cf6983035dc9
Max Molema <39056898+maxmolema@users.noreply.github.com>
2020-06-02 15:28:34 +0200

    Added Shell Scripts
    Added Shell Scripts for installation, auto-update and autorun.

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#This script autostarts the Signage System on boot.
+
+if [ "$EUID" -ne 0 ]
+  then echo Run as root
+  exit
+fi
+apt-get install unclutter
+cp signage-autostart.desktop /etc/xdg/autostart
+echo Done
---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#This script updates and launches the Signage System.
+#It assumes you have installed NodeJS.
+echo Running Updates.
+#Hide cursor and taskbar
+unclutter -idle 0 &
+pkill lxpanel
+cd $HOME
+if [[ ! -d signage ]] 
+then
+    mkdir signage
+fi
+cd signage
+echo Getting latest version from Github.
+git clone git://github.com/maxmolema/signage tmp
+rsync -a tmp/* .
+rm -rf tmp
+if [[ ! -d content/slides ]] 
+then
+    mkdir content/slides
+fi
+pcmanfm --set-wallpaper $HOME/signage/content/bootimg/boot.png
+echo Installing latest node packages.
+npm install
+echo Starting App
+npm start
